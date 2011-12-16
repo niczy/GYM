f97d53edda47c929fd2dab833dc617c159216faf
Administrator <nicholas@ubuntu.ubuntu-domain>
2011-12-16 17:50:26 +0800

    MOD: add merge-css.py

--- makefile
+++ makefile
@@ -2,6 +2,8 @@ dev:
 	if [ -d static/js ] ; then rm -r static/js ; fi
 	mkdir static/js ;
 	cp -r scripts/* static/js ;
-	lessc stylesheets/gym.less > static/css/gym.css
+	python stylesheets/merge-css.py > gym.less ;
+	lessc gym.less > static/css/gym.css ;
+	rm gym.less ;
 	dev_appserver.py . ;
 
---
+++ stylesheets/merge-css.py
@@ -0,0 +1,20 @@
+import os
+import stat
+
+def walktree(current_folder= '.'):
+    for file_name in os.listdir(current_folder):
+	st = os.lstat(os.path.join(current_folder, file_name))
+	if stat.S_ISDIR(st.st_mode):
+	    walktree(os.path.join(current_folder, file_name))
+	else:
+	    if file_name=='gym.less':
+		continue
+
+	    name, extension = os.path.splitext(file_name)
+	    if not extension == '.less':
+		continue
+
+	    print "@import \"%s\";" % (os.path.join(current_folder, file_name))
+
+if __name__ == '__main__':
+    walktree()
