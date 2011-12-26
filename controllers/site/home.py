a8921a6c91c0eb11bf0604488691c2d18c954d82
Xiao Zhang <zero891109@gmail.com>
2011-12-26 16:56:18 +0800

    Re-org code using inheritance, extract control bar as a seperate class

--- controllers/site/home.py
+++ controllers/site/home.py
@@ -32,7 +32,7 @@ class TestMenu(JSONRequestHandler):
         status: "done"
         '''
         
-        self.response_json('{"testitems":['
+        self.response_json('{"items":['
                       '{"testid":"1","title":"Kaplan 1.0","viewer":1421,"buyer":12,"uploader":"KP","uploadTime":"2011-12-03","rating":3.8,"price":0,"status":"done","uploaderPortrait":"/static/images/portraits/ETS.png"},'
                       '{"testid":"2","title":"Kaplan 2","viewer":134,"buyer":56,"uploader":"KP","uploadTime":"2011-11-03","rating":4.0,"price":10,"status":"new","uploaderPortrait":"/static/images/portraits/ETS.png"}'
-                      '],"name":"default","testnum":"16","description":"From Server"}')
+                      '],"itemnum":"16","description":"From Server"}')
--- scripts/apps/PracticeMenuPageApp.js
+++ scripts/apps/PracticeMenuPageApp.js
@@ -6,6 +6,8 @@ define(function(require, exports) {
 	require('../controls/tabs.js');
 	var PracticeMenuView = require('../views/menu/PracticeMenuView');
 	var PracticeMenuModel = require('../models/menu/PracticeMenuModel');
+	var MenuControlBar = require('../controls/MenuControlBar');
+
 	
 	var PracticeMenuPageApp = Backbone.Router.extend({
 		practice_type: 'reading',
@@ -16,64 +18,38 @@ define(function(require, exports) {
 			console.log('theApp.practice_type: ' + theApp.practice_type);
 			this.readingMenuModel = new PracticeMenuModel({
 				practice_type: 'reading', 
-				practiceitems: new Backbone.Collection()
+				items: new Backbone.Collection()
 			});
 			this.writingMenuModel = new PracticeMenuModel({
 				practice_type: 'writing', 
-				practiceitems: new Backbone.Collection()
+				items: new Backbone.Collection()
 			});
 			this.listeningMenuModel = new PracticeMenuModel({
 				practice_type: 'listening', 
-				practiceitems: new Backbone.Collection()
+				items: new Backbone.Collection()
 			});
 			this.speakingMenuModel = new PracticeMenuModel({
 				practice_type: 'speaking', 
-				practiceitems: new Backbone.Collection()
+				items: new Backbone.Collection()
 			});
-			//this.practiceMenuModel = this.readingMenuModel;
-			//testMenuModel = new TestMenuModel();
-			//this.practiceMenuModel.fetch({success:function(){console.log("Success!");}});
+
 			this.practiceMenuView = new PracticeMenuView({
 				model: this.practiceMenuModel(),
 				el: '#practice-menu'
 			});
-			//this.practiceMenuView.render();
-			
-			var GetSortField = function() {
-				var select = $("#menu-order-select");
-		        var str = "";
-		        $("select option:selected").each(function () {
-		              str = $(this).attr('value');
-		        });
-		        return str;
-			};
-			var that = this;
-			var SortMenuBy = function(field) {
-
-	            console.log("Sort By: " + field);
-	            theApp.practiceMenuModel().get('practiceitems').comparator = function(item) {
-	        	      if (!that.reverse) {
-	        	    	      return item.get(field);
-	        	      } else {
-	        	    	      return -item.get(field);
-	        	      }
-	            };
-	            theApp.practiceMenuModel().get('practiceitems').sort();
-	            theApp.practiceMenuView.render();      
-			};
-			
-			
-			$("#reverse-menu").click(function() {
-				that.reverse = !that.reverse;
-				SortMenuBy(GetSortField());
-			});
-
-			$("#menu-order-select").change(function () {
-				SortMenuBy(GetSortField());
-	        }).trigger('change');
 
 			$("#subnav-bar > ul > li").click(function() {
 				theApp.navigate($(this).attr("name"), true);
+			});
+			
+			var controlBar = new MenuControlBar({
+				getModel: function() {
+					return theApp.practiceMenuModel();
+				},
+				el: '#control-bar',
+				options: [
+				   {key: 'practiceid', value: 'Default'},
+				   {key: 'viewer', value: 'Num of Views'}]
 			});
 		},
 		getModelByType: function(type) {
@@ -103,17 +79,19 @@ define(function(require, exports) {
  			});
 			
 			var theModel = this.getModelByType(type);
-			//this.practiceMenuModel = theModel;
-			if (!theModel.get('fetched')) {
-				theModel.fetch();
-				theModel.set({'fetched': true});
-			} 
 			console.log('ResetView');
 			this.practiceMenuView = new PracticeMenuView({
 				model: theModel,
 				el: '#practice-menu'
 			});
-			this.practiceMenuView.render();
+			//this.practiceMenuModel = theModel;
+			if (!theModel.get('fetched')) {
+				theModel.fetch();
+				theModel.set({'fetched': true});
+			} else {
+				theModel.change();
+				//this.practiceMenuView.render();
+			}
 		}
 	});
 	var app = new PracticeMenuPageApp;
